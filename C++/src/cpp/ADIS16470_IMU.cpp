bdbbeb924d5f597c956299e0737b1f126a9e5aa7
Juan Chong <juan@highcurrent.io>
2018-12-28 01:06:31 -0500

    Updated the driver to match 448 capabilities (minus AHRS).

---
+++ C++/src/cpp/ADIS16470_IMU.cpp
@@ -0,0 +1,404 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2016-2018 FIRST. All Rights Reserved.                        */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*                                                                            */
+/* Modified by Juan Chong - juan.chong@analog.com                             */
+/*----------------------------------------------------------------------------*/
+
+#include <string>
+#include <iostream>
+#include <cmath>
+
+#include <frc/DigitalInput.h>
+#include <frc/DigitalSource.h>
+#include <frc/DriverStation.h>
+#include <frc/ErrorBase.h>
+#include <ADIS16470_IMU.h>
+#include <frc/smartdashboard/SendableBuilder.h>
+#include <frc/Timer.h>
+#include <frc/WPIErrors.h>
+#include <hal/HAL.h>
+
+static constexpr double kCalibrationSampleTime = 5.0; // Calibration time in seconds (regardless of sps)
+static constexpr double kDegreePerSecondPerLSB = 1.0/10.0;
+static constexpr double kGPerLSB = 1.0/800.0;
+static constexpr double kDegCPerLSB = 1.0/10.0;
+static constexpr double kDegCOffset = 0.0;
+
+static constexpr uint8_t kGLOB_CMD = 0x68;
+static constexpr uint8_t kRegDEC_RATE = 0x64;
+static constexpr uint8_t kRegFILT_CTRL = 0x5C;
+static constexpr uint8_t kRegMSC_CTRL = 0x60;
+static constexpr uint8_t kRegPROD_ID = 0x72;
+
+double timestamp_old = 0;
+
+static inline uint16_t ToUShort(const uint8_t* buf) {
+  return ((uint16_t)(buf[0]) << 8) | buf[1];
+}
+
+static inline int16_t ToShort(const uint8_t* buf) {
+  return (int16_t)(((uint16_t)(buf[0]) << 8) | buf[1]);
+}
+
+using namespace frc;
+
+/**
+ * Constructor.
+ */
+ADIS16470_IMU::ADIS16470_IMU() : ADIS16470_IMU(kZ, SPI::Port::kOnboardCS0) {}
+
+ADIS16470_IMU::ADIS16470_IMU(IMUAxis yaw_axis, SPI::Port port) : m_yaw_axis(yaw_axis), m_spi(port) {
+  
+/*   // Force the IMU reset pin to toggle on startup (doesn't require DS enable)
+  DigitalOutput *m_reset_out = new DigitalOutput(18);  // Drive MXP DIO8 low
+  Wait(0.01);  // Wait 10ms
+  m_reset_out -> ~DigitalOutput();
+  DigitalInput *m_reset_in = new DigitalInput(18);  // Set MXP DIO8 high
+  m_reset_in -> ~DigitalInput(); */
+  Wait(0.5);  // Wait 500ms
+
+  // Set general SPI settings
+  m_spi.SetClockRate(1000000);
+  m_spi.SetMSBFirst();
+  m_spi.SetSampleDataOnTrailingEdge();
+  m_spi.SetClockActiveLow();
+  m_spi.SetChipSelectActiveLow();
+
+  ReadRegister(kRegPROD_ID); // Dummy read
+
+  // Validate the product ID
+  if (ReadRegister(kRegPROD_ID) != 16982) {
+    DriverStation::ReportError("Could not find ADIS16470!");
+    return;
+  }
+  else {
+    DriverStation::ReportError("ADIS16470 IMU Detected. Starting calibration.");
+  }
+
+  // Set IMU internal decimation to 200 SPS
+  WriteRegister(kRegDEC_RATE, 0x0009);
+
+  // Enable Data Ready (LOW = Good Data), gSense Compensation, PoP
+  WriteRegister(kRegMSC_CTRL, 0x00C1);
+
+  // Configure IMU internal Bartlett filter
+  WriteRegister(kRegFILT_CTRL, 0x0000);
+
+  // Configure interrupt on SPI CS1
+  DigitalInput *m_interrupt = new DigitalInput(26);
+
+  // Configure DMA SPI
+  m_spi.InitAuto(8200);
+  m_spi.SetAutoTransmitData(kGLOB_CMD,21);
+
+  // Kick off DMA SPI (Note: Device configration impossible after SPI DMA is activated)
+  m_spi.StartAutoTrigger(*m_interrupt,true,false);
+
+  // Start acquisition and calculation threads
+  m_freed = false;
+  m_acquire_task = std::thread(&ADIS16470_IMU::Acquire, this);
+
+  // Execute offset calibration on start-up
+  Calibrate();
+
+  Wait(0.500); 
+
+  // Re-initialize accumulations after calibration
+  Reset();
+  
+  // Let the user know the IMU was initiallized successfully
+  DriverStation::ReportError("ADIS16470 IMU Successfully Initialized!");
+
+  // Report usage and post data to DS
+  HAL_Report(HALUsageReporting::kResourceType_ADIS16448, 0);
+  //HAL_Report(HALUsageReporting::kResourceType_ADIS16470, 0);
+  SetName("ADIS16470", 0);
+}
+
+void ADIS16470_IMU::Calibrate() {
+  {
+    std::lock_guard<wpi::mutex> sync(m_mutex);
+    m_accum_count = 0;
+    m_accum_gyro_x = 0.0;
+    m_accum_gyro_y = 0.0;
+    m_accum_gyro_z = 0.0;
+  }
+
+  Wait(kCalibrationSampleTime);
+  {
+    std::lock_guard<wpi::mutex> sync(m_mutex);
+    m_gyro_offset_x = m_accum_gyro_x / m_accum_count;
+    m_gyro_offset_y = m_accum_gyro_y / m_accum_count;
+    m_gyro_offset_z = m_accum_gyro_z / m_accum_count;
+  }
+}
+
+uint16_t ADIS16470_IMU::ReadRegister(uint8_t reg) {
+  uint8_t buf[2];
+  buf[0] = reg & 0x7f;
+  buf[1] = 0;
+
+  m_spi.Write(buf, 2);
+  m_spi.Read(false, buf, 2);
+
+  return ToUShort(buf);
+}
+
+void ADIS16470_IMU::WriteRegister(uint8_t reg, uint16_t val) {
+  uint8_t buf[2];
+  buf[0] = 0x80 | reg;
+  buf[1] = val & 0xff;
+  m_spi.Write(buf, 2);
+  buf[0] = 0x81 | reg;
+  buf[1] = val >> 8;
+  m_spi.Write(buf, 2);
+}
+
+void ADIS16470_IMU::Reset() {
+  std::lock_guard<wpi::mutex> sync(m_mutex);
+  m_integ_gyro_x = 0.0;
+  m_integ_gyro_y = 0.0;
+  m_integ_gyro_z = 0.0;
+}
+
+ADIS16470_IMU::~ADIS16470_IMU() {
+  m_spi.StopAuto();
+  m_freed = true;
+  m_samples_not_empty.notify_all();
+  if (m_acquire_task.joinable()) m_acquire_task.join();
+}
+
+void ADIS16470_IMU::Acquire() {
+  uint32_t buffer[2000];
+  double gyro_x, gyro_y, gyro_z, accel_x, accel_y, accel_z, temp, status, counter;
+  int data_count = 0;
+  int data_remainder = 0;
+  int data_to_read = 0;
+  uint8_t data_subset[24];
+  uint32_t timestamp_new = 0;
+
+  while (!m_freed) {
+
+	  // Waiting for the buffer to fill...
+	  Wait(.020); // A delay greater than 50ms could potentially overflow the local buffer (depends on sensor sample rate)
+    
+    std::fill_n(buffer, 2000, 0);  // Clear buffer
+	  data_count = m_spi.ReadAutoReceivedData(buffer,0,0); // Read number of bytes currently stored in the buffer
+	  data_remainder = data_count % 23; // Check if frame is incomplete
+    data_to_read = data_count - data_remainder;  // Remove incomplete data from read count
+	  m_spi.ReadAutoReceivedData(buffer,data_to_read,0); // Read data from DMA buffer
+
+    /* // DEBUG: Print buffer size and contents to terminal
+    std::cout << "Start - " << data_count << "," << data_remainder << "," << data_to_read << std::endl;
+    for (int m = 0; m < data_to_read - 1; m++ )
+    {
+      std::cout << buffer[m] << ",";
+    }
+    std::cout << " " << std::endl;
+    std::cout << "End" << std::endl; */
+
+	  for (int i = 0; i < data_to_read; i += 23) { // Process each set of 22 bytes + timestamp (23 total)
+
+		  for (int j = 1; j < 23; j++) { 
+			  data_subset[j - 1] = buffer[i + j];  // Split each set of 22 bytes into a sub-array for processing
+      }
+
+		  // DEBUG: Plot sub-array data in terminal
+ 		  /*std::cout << ToUShort(&data_subset[0]) << "," << ToUShort(&data_subset[2]) << "," << ToUShort(&data_subset[4]) <<
+		  "," << ToUShort(&data_subset[6]) << "," << ToUShort(&data_subset[8]) << "," << ToUShort(&data_subset[10]) << "," <<
+		  ToUShort(&data_subset[12]) << "," << ToUShort(&data_subset[14]) << "," << ToUShort(&data_subset[16]) << "," <<
+		  ToUShort(&data_subset[18]) << "," << ToUShort(&data_subset[20]) << std::endl; */
+
+      // Calculate checksum on each data packet
+      uint16_t imu_checksum = 0;
+      uint16_t calc_checksum = 0;
+		  for(int k = 2; k < 20; k++ ) { // Cycle through STATUS, XYZ GYRO, XYZ ACCEL, TEMP, COUNTER (Ignore DUT Checksum)
+			  calc_checksum += data_subset[k];
+		  }
+
+		  imu_checksum = ToUShort(&data_subset[20]); // Extract DUT checksum from data buffer
+
+      // Compare calculated vs read CRC. Don't update outputs or dt if CRC-16 is bad
+      if (calc_checksum == imu_checksum) {
+        
+        // Calculate delta-time (dt) using FPGA timestamps
+        timestamp_new = buffer[i];  // Extract timestamp from buffer
+        dt = (timestamp_new - timestamp_old)/1000000; // Calculate dt and convert us to seconds
+        timestamp_old = timestamp_new; // Store new timestamp in old variable for next cycle
+
+        gyro_x = ToShort(&data_subset[4]) * kDegreePerSecondPerLSB;
+        gyro_y = ToShort(&data_subset[6]) * kDegreePerSecondPerLSB;
+        gyro_z = ToShort(&data_subset[8]) * kDegreePerSecondPerLSB;
+        accel_x = ToShort(&data_subset[10]) * kGPerLSB;
+        accel_y = ToShort(&data_subset[12]) * kGPerLSB;
+        accel_z = ToShort(&data_subset[14]) * kGPerLSB;
+        temp = ToShort(&data_subset[16]) * kDegCPerLSB + kDegCOffset;
+        status = ToUShort(&data_subset[2]);
+        counter = ToUShort(&data_subset[18]);
+
+        // Update global state
+        {
+          std::lock_guard<wpi::mutex> sync(m_mutex);
+          m_gyro_x = gyro_x;
+          m_gyro_y = gyro_y;
+          m_gyro_z = gyro_z;
+          m_accel_x = accel_x;
+          m_accel_y = accel_y;
+          m_accel_z = accel_z;
+          m_temp = temp;
+          m_status = status;
+          m_counter = counter;
+
+          // Accumulate gyro for offset calibration
+          ++m_accum_count;
+          m_accum_gyro_x += gyro_x;
+          m_accum_gyro_y += gyro_y;
+          m_accum_gyro_z += gyro_z;
+          
+          // Accumulate gyro for angle integration
+          m_integ_gyro_x += (gyro_x - m_gyro_offset_x) * dt;
+          m_integ_gyro_y += (gyro_y - m_gyro_offset_y) * dt;
+          m_integ_gyro_z += (gyro_z - m_gyro_offset_z) * dt;
+        }
+      }
+      else {
+        // Print notification when checksum fails and bad data is rejected
+        std::cout << "IMU Data Checksum Invalid - " << calc_checksum << "," << imu_checksum << " - Data Ignored and Integration Time Adjusted" << std::endl;
+      }
+	  }
+  }
+}
+
+double ADIS16470_IMU::GetAngle() const {
+  switch (m_yaw_axis) {
+    case kX: {
+      return GetAngleX();
+      break;
+    }
+    case kY: {
+      return GetAngleY();
+      break;
+    }
+    case kZ: {
+      return GetAngleZ();
+      break;
+    }
+  }
+}
+
+double ADIS16470_IMU::GetRate() const {
+  switch (m_yaw_axis) {
+    case kX: {
+      return GetRateX();
+      break;
+    }
+    case kY: {
+      return GetRateY();
+      break;
+    }
+    case kZ: {
+      return GetRateZ();
+      break;
+    }
+  }
+}
+
+double ADIS16470_IMU::GetAngleX() const {
+  std::lock_guard<wpi::mutex> sync(m_mutex);
+  return m_integ_gyro_x;
+}
+
+double ADIS16470_IMU::GetAngleY() const {
+  std::lock_guard<wpi::mutex> sync(m_mutex);
+  return m_integ_gyro_y;
+}
+
+double ADIS16470_IMU::GetAngleZ() const {
+  std::lock_guard<wpi::mutex> sync(m_mutex);
+  return m_integ_gyro_z;
+}
+
+double ADIS16470_IMU::GetRateX() const {
+  std::lock_guard<wpi::mutex> sync(m_mutex);
+  return m_gyro_x;
+}
+
+double ADIS16470_IMU::GetRateY() const {
+  std::lock_guard<wpi::mutex> sync(m_mutex);
+  return m_gyro_y;
+}
+
+double ADIS16470_IMU::GetRateZ() const {
+  std::lock_guard<wpi::mutex> sync(m_mutex);
+  return m_gyro_z;
+}
+
+double ADIS16470_IMU::GetAccelX() const {
+  std::lock_guard<wpi::mutex> sync(m_mutex);
+  return m_accel_x;
+}
+
+double ADIS16470_IMU::GetAccelY() const {
+  std::lock_guard<wpi::mutex> sync(m_mutex);
+  return m_accel_y;
+}
+
+double ADIS16470_IMU::GetAccelZ() const {
+  std::lock_guard<wpi::mutex> sync(m_mutex);
+  return m_accel_z;
+}
+
+double ADIS16470_IMU::Getdt() const {
+  std::lock_guard<wpi::mutex> sync(m_mutex);
+  return dt;
+}
+
+double ADIS16470_IMU::GetTemperature() const {
+  std::lock_guard<wpi::mutex> sync(m_mutex);
+  return m_temp;
+}
+
+double ADIS16470_IMU::GetStatus() const {
+  std::lock_guard<wpi::mutex> sync(m_mutex);
+  return m_status;
+}
+
+double ADIS16470_IMU::GetCounter() const {
+  std::lock_guard<wpi::mutex> sync(m_mutex);
+  return m_counter;
+}
+
+void ADIS16470_IMU::InitSendable(SendableBuilder& builder) {
+  builder.SetSmartDashboardType("ADIS16470 IMU");
+  auto gyroX = builder.GetEntry("GyroX").GetHandle();
+  auto gyroY = builder.GetEntry("GyroY").GetHandle();
+  auto gyroZ = builder.GetEntry("GyroZ").GetHandle();
+  auto accelX = builder.GetEntry("AccelX").GetHandle();
+  auto accelY = builder.GetEntry("AccelY").GetHandle();
+  auto accelZ = builder.GetEntry("AccelZ").GetHandle();
+  auto angleX = builder.GetEntry("AngleX").GetHandle();
+  auto angleY = builder.GetEntry("AngleY").GetHandle();
+  auto angleZ = builder.GetEntry("AngleZ").GetHandle();
+  auto temp = builder.GetEntry("Temperature").GetHandle();
+  auto dt = builder.GetEntry("dt").GetHandle();
+  auto status = builder.GetEntry("Status").GetHandle();
+  auto counter = builder.GetEntry("Counter").GetHandle();
+  builder.SetUpdateTable([=]() {
+	nt::NetworkTableEntry(gyroX).SetDouble(GetRateX());
+	nt::NetworkTableEntry(gyroY).SetDouble(GetRateY());
+	nt::NetworkTableEntry(gyroZ).SetDouble(GetRateZ());
+	nt::NetworkTableEntry(accelX).SetDouble(GetAccelX());
+	nt::NetworkTableEntry(accelY).SetDouble(GetAccelY());
+	nt::NetworkTableEntry(accelZ).SetDouble(GetAccelZ());
+	nt::NetworkTableEntry(angleX).SetDouble(GetAngleX());
+	nt::NetworkTableEntry(angleY).SetDouble(GetAngleY());
+	nt::NetworkTableEntry(angleZ).SetDouble(GetAngleZ());
+  nt::NetworkTableEntry(temp).SetDouble(GetTemperature());
+  nt::NetworkTableEntry(dt).SetDouble(Getdt());
+  nt::NetworkTableEntry(status).SetDouble(GetStatus());
+  nt::NetworkTableEntry(counter).SetDouble(GetCounter());
+  });
+}
---
+++ C++/src/include/ADIS16470_IMU.h
@@ -0,0 +1,321 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2016-2018 FIRST. All Rights Reserved.                        */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*                                                                            */
+/* Modified by Juan Chong - juan.chong@analog.com                             */
+/*----------------------------------------------------------------------------*/
+
+#pragma once
+
+#include <atomic>
+#include <cstdint>
+#include <memory>
+#include <thread>
+
+#include <frc/DigitalOutput.h>
+#include <frc/DigitalSource.h>
+#include <frc/DigitalInput.h>
+#include <frc/GyroBase.h>
+#include <frc/SPI.h>
+#include <frc/smartdashboard/SendableBuilder.h>
+#include <wpi/mutex.h>
+#include <wpi/condition_variable.h>
+
+namespace frc {
+
+/**
+ * Use DMA SPI to read rate and acceleration data from the ADIS16470 IMU and return the
+ * robot's heading relative to a starting position and instant measurements
+ *
+ * The ADIS16470 gyro angle outputs track the robot's heading based on the starting position. As
+ * the robot rotates the new heading is computed by integrating the rate of rotation returned by 
+ * the IMU. When the class is instantiated, a short calibration routine is performed where the 
+ * IMU samples the gyros while at rest to determine the initial offset. This is subtracted from 
+ * each sample to determine the heading.
+ *
+ * This class is for the ADIS16470 IMU connected via the primary SPI port available on the RoboRIO.
+ */
+
+class ADIS16470_IMU : public GyroBase {
+ public:
+
+ enum IMUAxis { kX, kY, kZ };
+
+  /**
+  * IMU constructor on onboard SPI CS0.
+  */
+  ADIS16470_IMU();
+
+  /**
+   * IMU constructor on the specified SPI port.
+   * 
+   * @param yaw_axis Selects the "default" axis to use for GetAngle() and GetRate()
+   * @param port The SPI port where the IMU is connected.
+   */
+  explicit ADIS16470_IMU(IMUAxis yaw_axis, SPI::Port port);
+
+  ~ADIS16470_IMU();
+
+  ADIS16470_IMU(ADIS16470_IMU&&) = default;
+  ADIS16470_IMU& operator=(ADIS16470_IMU&&) = default;
+  
+  /**
+   * Initialize the IMU.
+   *
+   * Perform gyro offset calibration by collecting data for a number of seconds and 
+   * computing the center value. The center value is subtracted from subsequent
+   * measurements. 
+   *
+   * It's important to make sure that the robot is not moving while the
+   * centering calculations are in progress, this is typically done when the
+   * robot is first turned on while it's sitting at rest before the match
+   * starts.
+   * 
+   * The calibration routine can be triggered by the user during runtime.
+   */
+  void Calibrate() override;
+
+  /**
+   * Reset the gyro.
+   *
+   * Resets the gyro accumulations to a heading of zero. This can be used if 
+   * there is significant drift in the gyro and it needs to be recalibrated
+   * after running.
+   */
+  void Reset() override;
+
+  /**
+   * Return the actual angle in degrees that the robot is currently facing.
+   *
+   * The angle is based on the current accumulator value corrected by
+   * offset calibration and built-in IMU calibration. The angle is continuous, 
+   * that is it will continue from 360->361 degrees. This allows algorithms 
+   * that wouldn't want to see a discontinuity in the gyro output as it sweeps 
+   * from 360 to 0 on the second time around. The axis returned by this 
+   * function is adjusted fased on the configured yaw_axis.
+   *
+   * @return the current heading of the robot in degrees. This heading is based
+   *         on integration of the returned rate from the gyro.
+   */
+  double GetAngle() const override;
+
+  /**
+   * Return the rate of rotation of the yaw_axis gyro.
+   *
+   * The rate is based on the most recent reading of the gyro value
+   *
+   * @return the current rate in degrees per second
+   */
+  double GetRate() const override;
+
+  /**
+   * Return the IMU X-axis integrated angle in degrees.
+   *
+   * The angle is based on the current accumulator value corrected by
+   * offset calibration and built-in IMU calibration. The angle is continuous, 
+   * that is it will continue from 360->361 degrees. This allows algorithms 
+   * that wouldn't want to see a discontinuity in the gyro output as it sweeps 
+   * from 360 to 0 on the second time around. 
+   *
+   * @return the current accumulated value of the X-axis in degrees. 
+   */
+  double GetAngleX() const;
+
+  /**
+   * Return the IMU Y-axis integrated angle in degrees.
+   *
+   * The angle is based on the current accumulator value corrected by
+   * offset calibration and built-in IMU calibration. The angle is continuous, 
+   * that is it will continue from 360->361 degrees. This allows algorithms 
+   * that wouldn't want to see a discontinuity in the gyro output as it sweeps 
+   * from 360 to 0 on the second time around. 
+   *
+   * @return the current accumulated value of the Y-axis in degrees. 
+   */
+  double GetAngleY() const;
+
+  /**
+   * Return the IMU Z-axis integrated angle in degrees.
+   *
+   * The angle is based on the current accumulator value corrected by
+   * offset calibration and built-in IMU calibration. The angle is continuous, 
+   * that is it will continue from 360->361 degrees. This allows algorithms 
+   * that wouldn't want to see a discontinuity in the gyro output as it sweeps 
+   * from 360 to 0 on the second time around. 
+   *
+   * @return the current accumulated value of the Z-axis in degrees. 
+   */
+  double GetAngleZ() const;
+
+  /**
+   * Return the rate of rotation of the X-axis gyro.
+   *
+   * The rate is based on the most recent reading of the gyro value
+   *
+   * @return the current rate of the X-axis gyro in degrees per second
+   */
+  double GetRateX() const;
+
+  /**
+   * Return the rate of rotation of the Y-axis gyro.
+   *
+   * The rate is based on the most recent reading of the gyro value
+   *
+   * @return the current rate of the Y-axis gyro in degrees per second
+   */
+  double GetRateY() const;
+
+  /**
+   * Return the rate of rotation of the Z-axis gyro.
+   *
+   * The rate is based on the most recent reading of the gyro value
+   *
+   * @return the current rate of the Z-axis gyro in degrees per second
+   */
+  double GetRateZ() const;
+
+  /**
+   * Return acceleration of the X-axis accelerometer.
+   *
+   * The acceleration measurement is based on the most recent reading of 
+   * the accelerometer value.
+   *
+   * @return the current acceleration of the X-axis accelerometer in g's
+   */
+  double GetAccelX() const;
+
+  /**
+   * Return acceleration of the Y-axis accelerometer.
+   *
+   * The acceleration measurement is based on the most recent reading of 
+   * the accelerometer value.
+   *
+   * @return the current acceleration of the Y-axis accelerometer in g's
+   */
+  double GetAccelY() const;
+
+  /**
+   * Return acceleration of the Z-axis accelerometer.
+   *
+   * The acceleration measurement is based on the most recent reading of 
+   * the accelerometer value.
+   *
+   * @return the current acceleration of the Z-axis accelerometer in g's
+   */
+  double GetAccelZ() const;
+
+  /**
+   * Return the delta-time calculation.
+   *
+   * The delta-time value is calculated by subtracting the previous data timestamp from the
+   * current timestamp. 
+   *
+   * @return the delta-time calculation of each data capture in seconds
+   */
+  double Getdt() const;
+
+  /**
+   * Return temperature as measured by the IMU.
+   *
+   * The temperature measurement is based on the most recent reading of the temperature sensor
+   * built in to the inertial sensors. 
+   * 
+   * @return the current temperature in degrees C
+   */
+  double GetTemperature() const;
+
+  /**
+   * Return IMU status.
+   *
+   * IMU status can be deciphered by referring to Table 10 in the datasheet.
+   * 
+   * @return the status code read from the IMU
+   */
+  double GetStatus() const;
+
+  /**
+   * Return IMU sample counter.
+   *
+   * Sample counter is incremented every time IMU data is made available on the SPI bus. 
+   * Output counts up from 0 to 65534 and rolls over to 0 continuously.
+   * 
+   * @return the sample counter
+   */
+  double GetCounter() const;
+
+  void InitSendable(SendableBuilder& builder) override;
+
+ private:
+  // Sample from the IMU
+  struct Sample {
+    double gyro_x;
+    double gyro_y;
+    double gyro_z;
+    double accel_x;
+    double accel_y;
+    double accel_z;
+    double temp;
+    double status;
+    double counter;
+    double dt;
+  };
+
+  // IMU yaw axis
+  IMUAxis m_yaw_axis;
+
+  uint16_t ReadRegister(uint8_t reg);
+  void WriteRegister(uint8_t reg, uint16_t val);
+  void Acquire();
+
+  // Gyro offset
+  double m_gyro_offset_x = 0.0;
+  double m_gyro_offset_y = 0.0;
+  double m_gyro_offset_z = 0.0;
+
+  // Last read values (post-scaling)
+  double m_gyro_x = 0.0;
+  double m_gyro_y = 0.0;
+  double m_gyro_z = 0.0;
+  double m_accel_x = 0.0;
+  double m_accel_y = 0.0;
+  double m_accel_z = 0.0;
+  double m_temp = 0.0;
+  double m_status = 0.0;
+  double m_counter = 0.0;
+  double dt = 0.0;
+
+  // Accumulated gyro values (for offset calculation)
+  int m_accum_count = 0;
+  double m_accum_gyro_x = 0.0;
+  double m_accum_gyro_y = 0.0;
+  double m_accum_gyro_z = 0.0;
+
+  // Integrated gyro values
+  double m_integ_gyro_x = 0.0;
+  double m_integ_gyro_y = 0.0;
+  double m_integ_gyro_z = 0.0;
+
+  std::atomic_bool m_freed;
+
+  SPI m_spi;
+  std::unique_ptr<frc::DigitalOutput> m_reset_out;
+  std::unique_ptr<frc::DigitalInput> m_reset_in;
+  std::unique_ptr<frc::DigitalSource> m_interrupt;
+
+  std::thread m_acquire_task;
+
+  mutable wpi::mutex m_mutex;
+
+  // Samples FIFO.  We make the FIFO 2 longer than it needs
+  // to be so the input and output never overlap (we hold a reference
+  // to the output while the lock is released).
+  static constexpr int kSamplesDepth = 10;
+  Sample m_samples[kSamplesDepth + 2];
+  wpi::mutex m_samples_mutex;
+  wpi::condition_variable m_samples_not_empty;
+
+};
+
+} //namespace frc
